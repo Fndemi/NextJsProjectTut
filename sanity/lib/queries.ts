6b47f141b9ba51cd28998800c1f8eb6cfae41ecc
Florence Ndemi <ndemiflorence@gmail.com>
2025-08-29 12:59:39 +0300

    Added dynamic and static parts in the startup page

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -16,3 +16,10 @@ export const STARTUP_BY_ID_QUERY = defineQuery(
     },views,description,category,image,pitch
 }`
 );
+
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+  *[_type == "startup" && _id == $id][0]{
+    _id,
+    views
+  }
+`);
